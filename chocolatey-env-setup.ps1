85a1b186cf42b0ce41479901f0c0e69c92afa46c
anton_evtikhov <anton_evtikhov@epam.com>
2019-04-08 11:50:39 +0300

    add script

---
+++ chocolatey-env-setup.ps1
@@ -0,0 +1,17 @@
+# Simple environment setup script
+
+# Install Applications
+choco install notepadplusplus
+choco install intellijidea-community
+choco install lightshot 
+choco install GoogleChrome
+choco install putty
+choco install keepass
+choco install skype
+choco install git
+choco install 7zip
+choco install sysinternals
+choco install treesize
+choco install telegram
+choco install mremoteng
+
